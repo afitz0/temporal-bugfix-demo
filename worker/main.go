7947feee855ffb4dadb47b6d0819f20881e61531
Andrew Fitz Gibbon <fitz@temporal.io>
2023-04-24 08:55:50 -0600

    new project

---
+++ worker/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+
+	"starter"
+	"starter/zapadapter"
+)
+
+func main() {
+	c, err := client.NewLazyClient(client.Options{
+		Logger: zapadapter.NewZapAdapter(
+			zapadapter.NewZapLogger()),
+	})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "temporal-starter", worker.Options{})
+
+	a := &starter.Activities{}
+	w.RegisterWorkflow(starter.Workflow)
+	w.RegisterActivity(a)
+
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
